5935419bba21bdf21958ca49e2cc3b63b59c7c6a
ChildishhAlbino <connorwilliams.home@gmail.com>
2023-11-15 01:37:01 +1100

    feat: allow for absolute or relative paths

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 import { visit } from 'unist-util-visit'
-export default function remarkObsidianImg({ basePath }) {
-    return (tree) => {
-        visit(tree, (node) => {
-            console.log({ node })
+export default function remarkObsidianImg({ absolutePrefix, relativePathResolver }) {
 
+    if (!absolutePrefix && !relativePathResolver) {
+        throw Error("You must supply one path method.")
+    }
+
+    return (tree, file) => {
+        visit(tree, "text", (node) => {
             const matches = node.value?.match(/!\[\[(.*\..*)\]\]/)
             if (!matches?.[0]) {
                 return
             }
-            console.log({ matches });
-            const imagePath = matches[1]
+            const imageSrc = matches[1]
             node.type = "image"
-            node.url = `${basePath}/${imagePath}`
-            node.alt = imagePath
+            node.url = getPath(absolutePrefix, relativePathResolver, { filePath: file.history[0], cwd: file.cwd }, imageSrc)
+            node.alt = imageSrc
         })
     }
+}
+
+function getPath(absolutePrefix, relativePathResolver, fileDetails, imageSrc) {
+    if (absolutePrefix) {
+        return `${absolutePrefix}/${imageSrc}`
+    }
+    return relativePathResolver({ fileDetails, imageSrc })
 }
